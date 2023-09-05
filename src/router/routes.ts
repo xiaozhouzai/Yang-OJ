ca9bced8f90f31a97385b04043d286bae218f2cb
lcyzhy <lcyzhy@tom.com>
2023-09-05 09:11:43 +0800

    前端初始化(未完成)

---
+++ src/router/routes.ts
@@ -0,0 +1,42 @@
+import { RouteRecordRaw } from "vue-router";
+import UserLayout from "@/layouts/UserLayout.vue";
+import UserLoginView from "@/views/user/UserLoginView.vue";
+import UserRegisterView from "@/views/user/UserRegisterView.vue";
+import NoAuthView from "@/views/NoAuthView.vue";
+import QuestionsView from "@/views/question/QuestionsView.vue";
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginView,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterView,
+      },
+    ],
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/",
+    name: "主页",
+    component: QuestionsView,
+  },
+  {
+    path: "/noAuth",
+    name: "无权限",
+    component: NoAuthView,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+];
